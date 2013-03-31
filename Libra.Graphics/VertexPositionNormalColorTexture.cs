e3eceab5d3c840ca855c13a13cf0142f22996801
Will Craftia <willcraftia@gmail.com>
2013-04-01 02:14:19 +0900

    VertexPositionNormalColorTexture を追加。

---
+++ Libra.Graphics/VertexPositionNormalColorTexture.cs
@@ -0,0 +1,78 @@
+﻿#region Using
+
+using System;
+
+#endregion
+
+namespace Libra.Graphics
+{
+    public struct VertexPositionNormalColorTexture : IVertexType, IEquatable<VertexPositionNormalColorTexture>
+    {
+        public static readonly VertexDeclaration VertexDeclaration = new VertexDeclaration(
+            VertexElement.SVPosition, VertexElement.Normal, VertexElement.Color, VertexElement.TexCoord);
+
+        public Vector3 Position;
+
+        public Vector3 Normal;
+
+        public Color Color;
+
+        public Vector2 TexCoord;
+
+        public VertexPositionNormalColorTexture(Vector3 position, Vector3 normal, Color color, Vector2 texCoord)
+        {
+            Position = position;
+            Normal = normal;
+            Color = color;
+            TexCoord = texCoord;
+        }
+
+        VertexDeclaration IVertexType.VertexDeclaration
+        {
+            get { return VertexDeclaration; }
+        }
+
+        #region Equatable
+
+        public static bool operator ==(VertexPositionNormalColorTexture value1, VertexPositionNormalColorTexture value2)
+        {
+            return value1.Equals(value2);
+        }
+
+        public static bool operator !=(VertexPositionNormalColorTexture value1, VertexPositionNormalColorTexture value2)
+        {
+            return !value1.Equals(value2);
+        }
+
+        public bool Equals(VertexPositionNormalColorTexture other)
+        {
+            return Position == other.Position && Normal == other.Normal &&
+                Color == other.Color && TexCoord == other.TexCoord;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+
+            return Equals((VertexPositionNormalColorTexture) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Position.GetHashCode() ^ Normal.GetHashCode() ^
+                Color.GetHashCode() ^ TexCoord.GetHashCode();
+        }
+
+        #endregion
+
+        #region ToString
+
+        public override string ToString()
+        {
+            return "{Position:" + Position + " Normal:" + Normal +
+                " Color:" + Color + " TexCoord:" + TexCoord + "}";
+        }
+
+        #endregion
+    }
+}
