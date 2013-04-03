0204e3e86ee11e9d676f93555101246fda8a07f2
Will Craftia <willcraftia@gmail.com>
2013-04-03 23:04:15 +0900

    ガウス ブラーを簡単に適用するためのヘルパ クラスを実装。

--- Libra.Graphics.Toolkit/GaussianBlur.cs
+++ Libra.Graphics.Toolkit/GaussianBlur.cs
@@ -2,185 +2,109 @@
 
 using System;
 
-#if DEBUG
-
-using Libra.Graphics.Compiler;
-
-#endif
-
 #endregion
 
 namespace Libra.Graphics.Toolkit
 {
-    //public sealed class GaussianBlur : IDisposable
-    //{
-    //    public const int MaxRadius = 7;
+    public sealed class GaussianBlur : IDisposable
+    {
+        DeviceContext context;
 
-    //    public const int MinRadius = 1;
+        GaussianBlurShader shader;
 
-    //    public const float MinAmount = 0.001f;
+        RenderTarget backingRenderTarget;
 
-    //    public const int DefaultRadius = 1;
+        SpriteBatch spriteBatch;
 
-    //    public const float DefaultAmount = 2;
+        public int Width { get; private set; }
 
-    //    RenderTarget backingRenderTarget;
+        public int Height { get; private set; }
 
-    //    SpriteBatch spriteBatch;
+        public SurfaceFormat Format { get; private set; }
 
-    //    IDevice device;
+        public int Radius
+        {
+            get { return shader.Radius; }
+            set { shader.Radius = value; }
+        }
 
-    //    public int Width { get; private set; }
+        public float Amount
+        {
+            get { return shader.Amount; }
+            set { shader.Amount = value; }
+        }
 
-    //    public int Height { get; private set; }
+        public GaussianBlur(DeviceContext context, int width, int height, SurfaceFormat format)
+        {
+            if (context == null) throw new ArgumentNullException("context");
+            if (width < 1) throw new ArgumentOutOfRangeException("width");
+            if (height < 1) throw new ArgumentOutOfRangeException("height");
 
-    //    public SurfaceFormat Format { get; private set; }
+            this.context = context;
+            Width = width;
+            Height = height;
 
-    //    public int Radius { get; private set; }
+            shader = new GaussianBlurShader(context);
+            shader.Width = width;
+            shader.Height = height;
+            
+            backingRenderTarget = context.Device.CreateRenderTarget();
+            backingRenderTarget.Width = width;
+            backingRenderTarget.Height = height;
+            backingRenderTarget.Format = format;
+            backingRenderTarget.Initialize();
 
-    //    public float Amount { get; private set; }
+            spriteBatch = new SpriteBatch(context);
+        }
 
-    //    public GaussianBlur(SpriteBatch spriteBatch, int width, int height, SurfaceFormat format)
-    //        : this(spriteBatch, width, height, format, DefaultRadius, DefaultAmount)
-    //    {
-    //    }
+        public void Filter(ShaderResourceView source, RenderTargetView destination)
+        {
+            Filter(source, backingRenderTarget.GetRenderTargetView(), BlurDirection.Horizon);
+            Filter(backingRenderTarget.GetShaderResourceView(), destination, BlurDirection.Vertical);
 
-    //    public GaussianBlur(SpriteBatch spriteBatch, int width, int height, SurfaceFormat format, int radius, float amount)
-    //    {
-    //        if (spriteBatch == null) throw new ArgumentNullException("spriteBatch");
-    //        if (width < 1) throw new ArgumentOutOfRangeException("width");
-    //        if (height < 1) throw new ArgumentOutOfRangeException("height");
-    //        if (radius < MinAmount || MaxRadius < radius) throw new ArgumentOutOfRangeException("value");
-    //        if (amount < MinAmount) throw new ArgumentOutOfRangeException("value");
+            context.SetRenderTarget(null);
+        }
 
-    //        this.effect = effect;
-    //        this.spriteBatch = spriteBatch;
-    //        Width = width;
-    //        Height = height;
-    //        Radius = radius;
-    //        Amount = amount;
+        void Filter(ShaderResourceView source, RenderTargetView destination, BlurDirection direction)
+        {
+            shader.Direction = direction;
 
-    //        device = effect.GraphicsDevice;
+            context.SetRenderTarget(destination);
+            
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Opaque, null, null, null, shader.Apply);
+            spriteBatch.Draw(source, destination.RenderTarget.Bounds, Color.White);
+            spriteBatch.End();
+        }
 
-    //        InitializeEffectParameters();
-    //        CacheEffectTechniques();
+        #region IDisposable
 
-    //        backingRenderTarget = new RenderTarget(device, width, height, false, format,
-    //            DepthFormat.None, 0, RenderTargetUsage.PlatformContents);
-    //    }
+        bool disposed;
 
-    //    public void Filter(RenderTarget source)
-    //    {
-    //        Filter(source, source);
-    //    }
+        ~GaussianBlur()
+        {
+            Dispose(false);
+        }
 
-    //    public void Filter(Texture2D source, RenderTarget destination)
-    //    {
-    //        Draw(horizontalBlurTechnique, source, backingRenderTarget);
-    //        Draw(verticalBlurTechnique, backingRenderTarget, destination);
-    //    }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
-    //    void Draw(EffectTechnique technique, Texture2D source, RenderTarget destination)
-    //    {
-    //        effect.CurrentTechnique = technique;
+        void Dispose(bool disposing)
+        {
+            if (disposed) return;
 
-    //        var samplerState = destination.GetPreferredSamplerState();
+            if (disposing)
+            {
+                shader.Dispose();
+                backingRenderTarget.Dispose();
+                spriteBatch.Dispose();
+            }
 
-    //        device.SetRenderTarget(destination);
-    //        spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Opaque, samplerState, null, null, effect);
-    //        spriteBatch.Draw(source, destination.Bounds, Color.White);
-    //        spriteBatch.End();
-    //        device.SetRenderTarget(null);
-    //    }
+            disposed = true;
+        }
 
-    //    void CacheEffectTechniques()
-    //    {
-    //        horizontalBlurTechnique = effect.Techniques["HorizontalBlur"];
-    //        verticalBlurTechnique = effect.Techniques["VerticalBlur"];
-    //    }
-
-    //    void InitializeEffectParameters()
-    //    {
-    //        effect.Parameters["KernelSize"].SetValue(Radius * 2 + 1);
-    //        PopulateWeights();
-    //        PopulateOffsetsH();
-    //        PopulateOffsetsV();
-    //    }
-
-    //    void PopulateWeights()
-    //    {
-    //        var weights = new float[Radius * 2 + 1];
-    //        var totalWeight = 0.0f;
-    //        var sigma = Radius / Amount;
-
-    //        int index = 0;
-    //        for (int i = -Radius; i <= Radius; i++)
-    //        {
-    //            weights[index] = MathHelper.CalculateGaussian(sigma, i);
-    //            totalWeight += weights[index];
-    //            index++;
-    //        }
-
-    //        // Normalize
-    //        for (int i = 0; i < weights.Length; i++)
-    //        {
-    //            weights[i] /= totalWeight;
-    //        }
-
-    //        effect.Parameters["Weights"].SetValue(weights);
-    //    }
-
-    //    void PopulateOffsetsH()
-    //    {
-    //        effect.Parameters["OffsetsH"].SetValue(CalculateOffsets(1 / (float) Width, 0));
-    //    }
-
-    //    void PopulateOffsetsV()
-    //    {
-    //        effect.Parameters["OffsetsV"].SetValue(CalculateOffsets(0, 1 / (float) Height));
-    //    }
-
-    //    Vector2[] CalculateOffsets(float dx, float dy)
-    //    {
-    //        var offsets = new Vector2[Radius * 2 + 1];
-
-    //        int index = 0;
-    //        for (int i = -Radius; i <= Radius; i++)
-    //        {
-    //            offsets[index] = new Vector2(i * dx, i * dy);
-    //            index++;
-    //        }
-
-    //        return offsets;
-    //    }
-
-    //    #region IDisposable
-
-    //    public void Dispose()
-    //    {
-    //        Dispose(true);
-    //        GC.SuppressFinalize(this);
-    //    }
-
-    //    bool disposed;
-
-    //    ~GaussianBlur()
-    //    {
-    //        Dispose(false);
-    //    }
-
-    //    void Dispose(bool disposing)
-    //    {
-    //        if (disposed) return;
-
-    //        if (disposing)
-    //        {
-    //            backingRenderTarget.Dispose();
-    //        }
-
-    //        disposed = true;
-    //    }
-
-    //    #endregion
-    //}
+        #endregion
+    }
 }
