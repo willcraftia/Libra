431a3bf6552020625c605bd693209021c968e156
Will Craftia <willcraftia@gmail.com>
2013-04-15 00:58:50 +0900

    破棄では先にエンジンの出力を停止。コメント記載。

--- Libra.Audio.SharpDX/SdxSoundEffectManager.cs
+++ Libra.Audio.SharpDX/SdxSoundEffectManager.cs
@@ -55,14 +55,30 @@ namespace Libra.Audio.SharpDX
         {
             if (disposing)
             {
-                MasteringVoice.DestroyVoice();
-                MasteringVoice.Dispose();
-
                 if (engineStarted)
                 {
+                    // 破棄の前にエンジンを停止させる必要はないと思われるが、
+                    // 念のため停止。
+                    // StopEngine() は出力を即時停止しているのみであり、
+                    // StartEngine() の再呼び出しにより再開可能な状態でしかない。
                     XAudio2.StopEngine();
                     engineStarted = false;
                 }
+
+                MasteringVoice.DestroyVoice();
+                MasteringVoice.Dispose();
+
+                // XAudio2 に関連付けられた全ての Voice を除去していない場合、
+                // XAudio2.Dispose() はキューにある全バッファの消費を待機しているように見える。
+                // 即座に破棄するには、先行して全ての Voice を除去する必要があると思われる。
+                // このため、マネージャの破棄では、
+                // マネージャから生成する SoundEffectInstance の参照を保持し、
+                // それらが保持する SourceVoice について DestroyVoice() を
+                // 先行して呼び出す手順としている。
+                // しかし、この手順は明示的な Dispose() 呼び出しでのみ保証でき、
+                // SharpDX 利用側クラスにとっての XAudio2 がマネージドの SharpDX クラスである以上は、
+                // GC からのファイナライザ呼び出しでは保証できない。
+
                 XAudio2.Dispose();
             }
 
