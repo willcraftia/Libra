35afde79575aa43bcbdd1300769a8857e83b5c24
Will Craftia <willcraftia@gmail.com>
2013-04-29 12:57:26 +0900

    コメント。コード整理。

--- Samples/Samples.ShadowMapping/MainGame.cs
+++ Samples/Samples.ShadowMapping/MainGame.cs
@@ -17,8 +17,16 @@ namespace Samples.ShadowMapping
     {
         #region DrawModelEffect
 
+        /// <summary>
+        /// モデル描画シェーダの簡易管理クラス。
+        /// </summary>
         sealed class DrawModelEffect
         {
+            #region Constants
+
+            /// <summary>
+            /// 定数バッファの定義。
+            /// </summary>
             struct Constants
             {
                 public Matrix World;
@@ -35,6 +43,8 @@ namespace Samples.ShadowMapping
 
                 public Vector4 AmbientColor;
             }
+
+            #endregion
 
             public Matrix World;
 
@@ -116,91 +126,225 @@ namespace Samples.ShadowMapping
 
         enum LightCameraType
         {
+            /// <summary>
+            /// LiSPSMLightCamera を用いる。
+            /// </summary>
             LiSPSM  = 0,
+            
+            /// <summary>
+            /// FocusedLightCamera を用いる。
+            /// </summary>
             Focused = 1,
+
+            /// <summary>
+            /// BasicLightCamera を用いる。
+            /// </summary>
             Basic   = 2,
         }
 
         #endregion
 
+        /// <summary>
+        /// シャドウ マップのサイズ (正方形)。
+        /// </summary>
         const int shadowMapSize = 2048;
 
+        /// <summary>
+        /// ウィンドウの幅。
+        /// </summary>
         const int windowWidth = 800;
 
+        /// <summary>
+        /// ウィンドウの高さ。
+        /// </summary>
         const int windowHeight = 480;
 
+        /// <summary>
+        /// Libra のグラフィックス マネージャ。
+        /// </summary>
         GraphicsManager graphicsManager;
 
+        /// <summary>
+        /// Libra の XNA コンテント マネージャ。
+        /// </summary>
         XnbManager content;
 
+        /// <summary>
+        /// スプライト バッチ。
+        /// </summary>
         SpriteBatch spriteBatch;
 
+        /// <summary>
+        /// フォント。
+        /// </summary>
         SpriteFont spriteFont;
 
+        /// <summary>
+        /// 表示カメラの位置。
+        /// </summary>
         Vector3 cameraPosition = new Vector3(0, 70, 100);
         
+        /// <summary>
+        /// 表示カメラの視線方向。
+        /// </summary>
         Vector3 cameraForward = new Vector3(0, -0.4472136f, -0.8944272f);
         
+        /// <summary>
+        /// 表示カメラの視錐台。
+        /// </summary>
         BoundingFrustum cameraFrustum = new BoundingFrustum(Matrix.Identity);
 
+        /// <summary>
+        /// 表示カメラの近平面までの距離。
+        /// </summary>
         float cameraNear = 1.0f;
 
+        /// <summary>
+        /// 表示カメラの遠平面までの距離。
+        /// </summary>
         float cameraFar = 1000.0f;
  
-        // ライトの進行方向 (XNA Shadow Mapping では原点から見たライトの方向)。
-        // 単位ベクトル。
+        /// <summary>
+        /// ライトの進行方向 (XNA Shadow Mapping では原点から見たライトの方向)。
+        /// 単位ベクトル。
+        /// </summary>
         Vector3 lightDirection = new Vector3(0.3333333f, -0.6666667f, -0.6666667f);
 
+        /// <summary>
+        /// ライトによる投影を処理する距離。
+        /// </summary>
         float lightFar = 500.0f;
 
+        /// <summary>
+        /// 前回の更新処理におけるキーボード状態。
+        /// </summary>
         KeyboardState lastKeyboardState = new KeyboardState();
 
+        /// <summary>
+        /// 前回の更新処理におけるジョイスティック状態。
+        /// </summary>
         JoystickState lastJoystickState = new JoystickState();
 
+        /// <summary>
+        /// 現在の更新処理におけるキーボード状態。
+        /// </summary>
         KeyboardState currentKeyboardState;
         
+        /// <summary>
+        /// 現在の更新処理におけるジョイスティック状態。
+        /// </summary>
         JoystickState currentJoystickState;
 
+        /// <summary>
+        /// シャドウ マップ エフェクト。
+        /// </summary>
         ShadowMapEffect shadowMapEffect;
 
-        DrawModelEffect drawModelShader;
+        /// <summary>
+        /// モデル描画エフェクト。
+        /// </summary>
+        DrawModelEffect drawModelEffect;
 
+        /// <summary>
+        /// グリッド モデル (格子状の床)。
+        /// </summary>
         Model gridModel;
 
+        /// <summary>
+        /// デュード モデル (人)。
+        /// </summary>
         Model dudeModel;
 
+        /// <summary>
+        /// 明示するシーン領域。
+        /// </summary>
         BoundingBox sceneBox;
 
+        /// <summary>
+        /// 表示カメラの視錐台をシーン領域として用いるか否かを示す値。
+        /// true (表示カメラの視錐台をシーン領域として用いる場合)、
+        /// false (sceneBox で明示した領域をシーン領域として用いる場合)。
+        /// </summary>
         bool useCameraFrustumSceneBox;
 
+        /// <summary>
+        /// 視錐台や境界ボックスの頂点を得るための一時作業配列。
+        /// </summary>
         Vector3[] corners;
 
+        /// <summary>
+        /// デュード モデルの回転量。
+        /// </summary>
         float rotateDude = 0.0f;
 
+        /// <summary>
+        /// 基礎的なシャドウ マップのレンダ ターゲット。
+        /// </summary>
         RenderTarget bsmRenderTarget;
 
+        /// <summary>
+        /// VSM のレンダ ターゲット。
+        /// </summary>
         RenderTarget vsmRenderTarget;
 
+        /// <summary>
+        /// 現在選択されているシャドウ マップのレンダ ターゲット。
+        /// </summary>
         RenderTarget currentShadowRenderTarget;
 
+        /// <summary>
+        /// デュード モデルに適用するワールド行列。
+        /// </summary>
         Matrix world;
         
+        /// <summary>
+        /// 表示カメラのビュー行列。
+        /// </summary>
         Matrix view;
         
+        /// <summary>
+        /// 表示カメラの射影行列。
+        /// </summary>
         Matrix projection;
 
+        /// <summary>
+        /// 基礎的な簡易ライト カメラ。
+        /// シーン領域へ焦点を合わせない。
+        /// XNA Shadow Mapping のライト カメラ算出と同程度の品質。
+        /// </summary>
         BasicLightCamera basicLightCamera;
 
+        /// <summary>
+        /// シーン領域へ焦点を合わせるライト カメラ。
+        /// 焦点合わせにより、BasicLightCamera よりも高品質となる。
+        /// </summary>
         FocusedLightCamera focusedLightCamera;
 
+        /// <summary>
+        /// LiSPSM ライト カメラ。
+        /// 焦点合わせおよび LiSPSM による補正により、
+        /// FocusedLightCamera よりも高品質となる。
+        /// </summary>
         LiSPSMLightCamera lispsmLightCamera;
 
+        /// <summary>
+        /// 現在選択されているライト カメラの種類。
+        /// </summary>
         LightCameraType currentLightCameraType;
 
+        /// <summary>
+        /// 現在のライト空間行列 (ビュー×射影行列)。
+        /// </summary>
         Matrix lightViewProjection;
 
+        /// <summary>
+        /// VSM で用いるガウシアン ブラー。
+        /// </summary>
         GaussianBlur gaussianBlur;
 
+        /// <summary>
+        /// 現在選択されているシャドウ マップの種類。
+        /// </summary>
         ShadowMapEffectForm shadowMapEffectForm;
 
         public MainGame()
@@ -219,9 +363,14 @@ namespace Samples.ShadowMapping
 
             // gridModel が半径約 183 であるため、
             // これを含むように簡易シーン AABB を決定。
+            // なお、広大な世界を扱う場合には、表示カメラの視錐台に含まれるオブジェクト、
+            // および、それらに投影しうるオブジェクトを動的に選択および決定し、
+            // 適切な最小シーン領域を算出して利用する。
             sceneBox = new BoundingBox(new Vector3(-200), new Vector3(200));
 
             useCameraFrustumSceneBox = true;
+
+            // ライト カメラの初期化。
 
             basicLightCamera = new BasicLightCamera();
             basicLightCamera.LightDirection = lightDirection;
@@ -246,7 +395,7 @@ namespace Samples.ShadowMapping
         protected override void LoadContent()
         {
             shadowMapEffect = new ShadowMapEffect(Device);
-            drawModelShader = new DrawModelEffect(Device);
+            drawModelEffect = new DrawModelEffect(Device);
 
             spriteBatch = new SpriteBatch(Device.ImmediateContext);
             spriteFont = content.Load<SpriteFont>("hudFont");
@@ -254,6 +403,7 @@ namespace Samples.ShadowMapping
             gridModel = content.Load<Model>("grid");
             dudeModel = content.Load<Model>("dude");
 
+            // 基礎的なシャドウ マップは R 値のみを用いる。
             bsmRenderTarget = Device.CreateRenderTarget();
             bsmRenderTarget.Width = shadowMapSize;
             bsmRenderTarget.Height = shadowMapSize;
@@ -261,6 +411,7 @@ namespace Samples.ShadowMapping
             bsmRenderTarget.DepthFormat = DepthFormat.Depth24Stencil8;
             bsmRenderTarget.Initialize();
 
+            // VSM は RG 値の二つを用いる。
             vsmRenderTarget = Device.CreateRenderTarget();
             vsmRenderTarget.Width = shadowMapSize;
             vsmRenderTarget.Height = shadowMapSize;
@@ -268,15 +419,19 @@ namespace Samples.ShadowMapping
             vsmRenderTarget.DepthFormat = DepthFormat.Depth24Stencil8;
             vsmRenderTarget.Initialize();
 
-            gaussianBlur = new GaussianBlur(Device, vsmRenderTarget.Width, vsmRenderTarget.Height, SurfaceFormat.Vector2);
+            // ガウシアン ブラーは VSM で用いるため、
+            // 内部で使用するレンダ ターゲットは VSM に合わせる。
+            gaussianBlur = new GaussianBlur(Device, vsmRenderTarget.Width, vsmRenderTarget.Height, vsmRenderTarget.Format);
             gaussianBlur.Radius = 4;
             gaussianBlur.Amount = 16;
         }
 
         protected override void Update(GameTime gameTime)
         {
+            // キーボード状態およびジョイスティック状態のハンドリング。
             HandleInput(gameTime);
 
+            // 表示カメラの更新。
             UpdateCamera(gameTime);
 
             base.Update(gameTime);
@@ -286,11 +441,14 @@ namespace Samples.ShadowMapping
         {
             var context = Device.ImmediateContext;
 
-            lightViewProjection = CreateLightViewProjectionMatrix();
+            // ライト カメラによるライト空間行列の算出。
+            lightViewProjection = CreateLightViewProjection();
 
+            // 念のため状態を初期状態へ。
             context.BlendState = BlendState.Opaque;
             context.DepthStencilState = DepthStencilState.Default;
 
+            // 選択されているシャドウ マップの種類に応じてレンダ ターゲットを切り替え。
             switch (shadowMapEffectForm)
             {
                 case ShadowMapEffectForm.Basic:
@@ -301,19 +459,24 @@ namespace Samples.ShadowMapping
                     break;
             }
 
+            // シャドウ マップの描画。
             CreateShadowMap();
 
+            // シャドウ マップを用いたシーンの描画。
             DrawWithShadowMap();
 
+            // シャドウ マップを画面左上に表示。
             DrawShadowMapToScreen();
 
+            // HUD のテキストを描画。
             DrawOverlayText();
 
             base.Draw(gameTime);
         }
 
-        Matrix CreateLightViewProjectionMatrix()
+        Matrix CreateLightViewProjection()
         {
+            // ライト カメラへ指定するシーン領域。
             BoundingBox actualSceneBox;
 
             if (useCameraFrustumSceneBox)
@@ -348,7 +511,7 @@ namespace Samples.ShadowMapping
             // カメラの行列を更新。
             lightCamera.Update(view, projection, actualSceneBox);
 
-            // ライトのビュー×射影行列。
+            // ライト空間行列の算出。
             Matrix lightViewProjection;
             Matrix.Multiply(ref lightCamera.LightView, ref lightCamera.LightProjection, out lightViewProjection);
 
@@ -359,15 +522,24 @@ namespace Samples.ShadowMapping
         {
             var context = Device.ImmediateContext;
 
+            // 選択中のシャドウ マップ レンダ ターゲットを設定。
             context.SetRenderTarget(currentShadowRenderTarget.GetRenderTargetView());
 
+            // レンダ ターゲットの R あるいは RG を 1 で埋める (1 は最遠の深度)。
+            // 同時に、深度ステンシルの深度も 1 へ。
             context.Clear(Color.White);
 
+            // デュード モデルのワールド行列。
             world = Matrix.CreateRotationY(MathHelper.ToRadians(rotateDude));
+            
+            // 投影オブジェクトとしてデュード モデルを描画。
+            // グリッド モデルは非投影オブジェクト。
             DrawModel(dudeModel, true);
 
+            // レンダ ターゲットをデフォルトへ戻す。
             context.SetRenderTarget(null);
 
+            // VSM を選択している場合はシャドウ マップへブラーを適用。
             if (shadowMapEffectForm == ShadowMapEffectForm.Variance)
             {
                 gaussianBlur.Filter(
@@ -383,11 +555,14 @@ namespace Samples.ShadowMapping
 
             context.Clear(Color.CornflowerBlue);
 
+            // シャドウ マップに対するサンプラ。
             context.PixelShaderSamplers[1] = SamplerState.PointClamp;
 
+            // シャドウ マップと共にグリッド モデルを描画。
             world = Matrix.Identity;
             DrawModel(gridModel, false);
 
+            // シャドウ マップと共にデュード モデルを描画。
             world = Matrix.CreateRotationY(MathHelper.ToRadians(rotateDude));
             DrawModel(dudeModel, false);
         }
@@ -398,6 +573,7 @@ namespace Samples.ShadowMapping
 
             if (createShadowMap)
             {
+                // シャドウ マップ エフェクトの準備。
                 shadowMapEffect.World = world;
                 shadowMapEffect.LightViewProjection = lightViewProjection;
                 shadowMapEffect.Form = shadowMapEffectForm;
@@ -405,18 +581,23 @@ namespace Samples.ShadowMapping
             }
             else
             {
-                drawModelShader.World = world;
-                drawModelShader.View = view;
-                drawModelShader.Projection = projection;
-                drawModelShader.LightViewProjection = lightViewProjection;
-                drawModelShader.LightDirection = lightDirection;
-                drawModelShader.DepthBias = 0.001f;
-                drawModelShader.AmbientColor = new Vector4(0.15f, 0.15f, 0.15f, 1.0f);
-                drawModelShader.ShadowMapEffectForm = shadowMapEffectForm;
-                drawModelShader.Apply(context);
+                // モデル描画エフェクトの準備。
+                drawModelEffect.World = world;
+                drawModelEffect.View = view;
+                drawModelEffect.Projection = projection;
+                drawModelEffect.LightViewProjection = lightViewProjection;
+                drawModelEffect.LightDirection = lightDirection;
+                drawModelEffect.DepthBias = 0.001f;
+                drawModelEffect.AmbientColor = new Vector4(0.15f, 0.15f, 0.15f, 1.0f);
+                drawModelEffect.ShadowMapEffectForm = shadowMapEffectForm;
+                drawModelEffect.Apply(context);
 
                 context.PixelShaderResources[1] = currentShadowRenderTarget.GetShaderResourceView();
             }
+
+            // モデルを描画。
+            // モデルは XNB 標準状態で読み込んでいるため、
+            // メッシュ パートのエフェクトには BasicEffect が設定されている。
 
             context.PrimitiveTopology = PrimitiveTopology.TriangleList;
 
@@ -429,6 +610,7 @@ namespace Samples.ShadowMapping
 
                     if (!createShadowMap)
                     {
+                        // BasicEffect に設定されているモデルのテクスチャを取得して設定。
                         context.PixelShaderResources[0] = (meshPart.Effect as BasicEffect).Texture;
                     }
 
@@ -439,6 +621,7 @@ namespace Samples.ShadowMapping
 
         void DrawShadowMapToScreen()
         {
+            // 現在のフレームで生成したシャドウ マップを画面左上に表示。
             spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Opaque, SamplerState.PointClamp);
             spriteBatch.Draw(currentShadowRenderTarget.GetShaderResourceView(), new Rectangle(0, 0, 128, 128), Color.White);
             spriteBatch.End();
@@ -446,6 +629,7 @@ namespace Samples.ShadowMapping
 
         void DrawOverlayText()
         {
+            // HUD のテキストを表示。
             var text = "B = Light camera type (" + currentLightCameraType + ")\n" +
                 "X = Shadow map form (" + shadowMapEffectForm + ")\n" +
                 "Y = Use camera frustum as scene box (" + useCameraFrustumSceneBox + ")\n" +
