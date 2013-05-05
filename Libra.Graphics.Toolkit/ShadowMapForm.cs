b938dc243fd6be55216f963564107bed51e0a101
Will Craftia <willcraftia@gmail.com>
2013-05-06 01:01:04 +0900

    コメント。

--- Libra.Graphics.Toolkit/ShadowMapForm.cs
+++ Libra.Graphics.Toolkit/ShadowMapForm.cs
@@ -6,9 +6,19 @@ using System;
 
 namespace Libra.Graphics.Toolkit
 {
+    /// <summary>
+    /// シャドウ マップの形式を表します。
+    /// </summary>
     public enum ShadowMapForm
     {
+        /// <summary>
+        /// 基礎的なシャドウ マップ。
+        /// </summary>
         Basic,
+
+        /// <summary>
+        /// 分散シャドウ マップ。
+        /// </summary>
         Variance
     }
 }
