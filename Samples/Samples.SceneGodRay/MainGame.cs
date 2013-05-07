b404daeb3b7aafe680201a372c5decc2c013d59c
Will Craftia <willcraftia@gmail.com>
2013-05-08 00:57:09 +0900

    GodRay サンプルの実装を開始。

---
+++ Samples/Samples.SceneGodRay/MainGame.cs
@@ -0,0 +1,29 @@
+﻿#region Using
+
+using System;
+using Libra.Games;
+
+#endregion
+
+namespace Samples.SceneGodRay
+{
+    public sealed class MainGame : Game
+    {
+    }
+
+    #region Program
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MainGame())
+            {
+                game.Run();
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ Samples/Samples.SceneGodRay/README.txt
@@ -0,0 +1,5 @@
+﻿Xbox LIVE Indie Games で公開されている Shadow Mapping サンプル コードを原型に、
+GodRay サンプルを作成。
+
+原型としての XNA Shadow Mapping サンプル
+http://xbox.create.msdn.com/en-US/education/catalog/sample/shadow_mapping_1
