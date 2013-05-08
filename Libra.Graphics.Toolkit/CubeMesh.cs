9e25a58c87f30b8a7e7af8e977073686bd933bd9
Will Craftia <willcraftia@gmail.com>
2013-05-09 00:01:42 +0900

    立方体メッシュを実装。

---
+++ Libra.Graphics.Toolkit/CubeMesh.cs
@@ -0,0 +1,55 @@
+﻿#region Using
+
+using System;
+
+#endregion
+
+namespace Libra.Graphics.Toolkit
+{
+    public sealed class CubeMesh : PrimitiveMesh
+    {
+        static readonly Vector3[] normals =
+        {
+            Vector3.Up,
+            Vector3.Down,
+            Vector3.Forward,
+            Vector3.Backward,
+            Vector3.Left,
+            Vector3.Right
+        };
+
+        public CubeMesh(Device device)
+            : this(device, 1)
+        {
+        }
+
+        public CubeMesh(Device device, float size)
+            : base(device)
+        {
+            var vertexCount = 6 * 4;
+            var indexCount = 6 * 6;
+            Allocate(vertexCount, indexCount);
+
+            foreach (Vector3 normal in normals)
+            {
+                var side1 = new Vector3(normal.Y, normal.Z, normal.X);
+                var side2 = Vector3.Cross(normal, side1);
+
+                AddIndex(CurrentVertex + 0);
+                AddIndex(CurrentVertex + 1);
+                AddIndex(CurrentVertex + 2);
+
+                AddIndex(CurrentVertex + 0);
+                AddIndex(CurrentVertex + 2);
+                AddIndex(CurrentVertex + 3);
+
+                AddVertex((normal - side1 - side2) * size / 2, normal);
+                AddVertex((normal - side1 + side2) * size / 2, normal);
+                AddVertex((normal + side1 + side2) * size / 2, normal);
+                AddVertex((normal + side1 - side2) * size / 2, normal);
+            }
+
+            Build();
+        }
+    }
+}
---
+++ Libra.Graphics.Toolkit/PrimitiveMesh.cs
@@ -0,0 +1,105 @@
+﻿#region Using
+
+using System;
+
+#endregion
+
+namespace Libra.Graphics.Toolkit
+{
+    public abstract class PrimitiveMesh : IDisposable
+    {
+        VertexPositionNormal[] vertices;
+
+        ushort[] indices;
+
+        int currentVertexCount;
+
+        int currentIndexCount;
+
+        public Device Device { get; private set; }
+
+        public VertexBuffer VertexBuffer { get; private set; }
+
+        public IndexBuffer IndexBuffer { get; private set; }
+
+        protected int CurrentVertex
+        {
+            get { return currentVertexCount; }
+        }
+
+        protected PrimitiveMesh(Device device)
+        {
+            if (device == null) throw new ArgumentNullException("device");
+
+            Device = device;
+        }
+
+        public void Draw(DeviceContext context)
+        {
+            if (context == null) throw new ArgumentNullException("context");
+
+            context.SetVertexBuffer(VertexBuffer);
+            context.IndexBuffer = IndexBuffer;
+            context.PrimitiveTopology = PrimitiveTopology.TriangleList;
+
+            context.DrawIndexed(IndexBuffer.IndexCount);
+        }
+
+        protected void Allocate(int vertexCount, int indexCount)
+        {
+            vertices = new VertexPositionNormal[vertexCount];
+            indices = new ushort[indexCount];
+        }
+
+        protected void AddVertex(Vector3 position, Vector3 normal)
+        {
+            vertices[currentVertexCount++] = new VertexPositionNormal(position, normal);
+        }
+
+        protected void AddIndex(int index)
+        {
+            if (ushort.MaxValue < (uint) index) throw new ArgumentOutOfRangeException("index");
+
+            indices[currentIndexCount++] = (ushort) index;
+        }
+
+        protected void Build()
+        {
+            VertexBuffer = Device.CreateVertexBuffer();
+            VertexBuffer.Usage = ResourceUsage.Immutable;
+            VertexBuffer.Initialize(vertices);
+
+            IndexBuffer = Device.CreateIndexBuffer();
+            IndexBuffer.Format = IndexFormat.SixteenBits;
+            IndexBuffer.Usage = ResourceUsage.Immutable;
+            IndexBuffer.Initialize(indices);
+        }
+
+        #region IDisposable
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        bool disposed;
+
+        ~PrimitiveMesh()
+        {
+            Dispose(false);
+        }
+
+        void Dispose(bool disposing)
+        {
+            if (disposed) return;
+
+            if (VertexBuffer != null) VertexBuffer.Dispose();
+            if (IndexBuffer != null) IndexBuffer.Dispose();
+
+            disposed = true;
+        }
+
+        #endregion
+    }
+}
