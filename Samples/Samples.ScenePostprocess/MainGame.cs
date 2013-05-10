cd9ed058ae73eb93efe804d6d287eaa9ef77bf31
Will Craftia <willcraftia@gmail.com>
2013-05-10 13:28:53 +0900

    各種ポストプロセスのサンプル実装を開始。

---
+++ Samples/Samples.ScenePostprocess/MainGame.cs
@@ -0,0 +1,239 @@
+﻿#region Using
+
+using System;
+using Libra;
+using Libra.Games;
+using Libra.Graphics;
+using Libra.Graphics.Toolkit;
+using Libra.Input;
+using Libra.Xnb;
+
+#endregion
+
+namespace Samples.ScenePostprocess
+{
+    public sealed class MainGame : Game
+    {
+        /// <summary>
+        /// ウィンドウの幅。
+        /// </summary>
+        const int WindowWidth = 800;
+
+        /// <summary>
+        /// ウィンドウの高さ。
+        /// </summary>
+        const int WindowHeight = 480;
+
+        /// <summary>
+        /// Libra のグラフィックス マネージャ。
+        /// </summary>
+        GraphicsManager graphicsManager;
+
+        /// <summary>
+        /// Libra の XNA コンテント マネージャ。
+        /// </summary>
+        XnbManager content;
+
+        /// <summary>
+        /// スプライト バッチ。
+        /// </summary>
+        SpriteBatch spriteBatch;
+
+        /// <summary>
+        /// フォント。
+        /// </summary>
+        SpriteFont spriteFont;
+
+        /// <summary>
+        /// 表示カメラ。
+        /// </summary>
+        BasicCamera camera;
+
+        /// <summary>
+        /// 前回の更新処理におけるキーボード状態。
+        /// </summary>
+        KeyboardState lastKeyboardState;
+
+        /// <summary>
+        /// 現在の更新処理におけるキーボード状態。
+        /// </summary>
+        KeyboardState currentKeyboardState;
+
+        /// <summary>
+        /// メッシュ描画のための基礎エフェクト。
+        /// </summary>
+        BasicEffect basicEffect;
+
+        /// <summary>
+        /// 立方体メッシュ。
+        /// </summary>
+        CubeMesh cubeMesh;
+
+        /// <summary>
+        /// 球メッシュ。
+        /// </summary>
+        SphereMesh sphereMesh;
+
+        /// <summary>
+        /// 円柱メッシュ。
+        /// </summary>
+        CylinderMesh cylinderMesh;
+
+        /// <summary>
+        /// 正方形メッシュ。
+        /// </summary>
+        SquareMesh squareMesh;
+
+        public MainGame()
+        {
+            graphicsManager = new GraphicsManager(this);
+
+            content = new XnbManager(Services, "Content");
+
+            graphicsManager.PreferredBackBufferWidth = WindowWidth;
+            graphicsManager.PreferredBackBufferHeight = WindowHeight;
+
+            camera = new BasicCamera
+            {
+                Position = new Vector3(0, 70, 100),
+                Direction = new Vector3(0, -0.4472136f, -0.8944272f),
+                Fov = MathHelper.PiOver4,
+                AspectRatio = (float) WindowWidth / (float) WindowHeight,
+                NearClipDistance = 1.0f,
+                FarClipDistance = 1000.0f
+            };
+            camera.Update();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(Device.ImmediateContext);
+            spriteFont = content.Load<SpriteFont>("hudFont");
+
+            basicEffect = new BasicEffect(Device);
+            basicEffect.AmbientLightColor = new Vector3(0.15f, 0.15f, 0.15f);
+            basicEffect.PerPixelLighting = true;
+            basicEffect.EnableDefaultLighting();
+
+            cubeMesh = new CubeMesh(Device, 20);
+            sphereMesh = new SphereMesh(Device, 20, 32);
+            cylinderMesh = new CylinderMesh(Device, 80, 20, 32);
+            squareMesh = new SquareMesh(Device, 400);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            // キーボード状態およびジョイスティック状態のハンドリング。
+            HandleInput(gameTime);
+
+            // 表示カメラの更新。
+            UpdateCamera(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            var context = Device.ImmediateContext;
+
+            // 念のため状態を初期状態へ。
+            context.BlendState = BlendState.Opaque;
+            context.DepthStencilState = DepthStencilState.Default;
+
+            // HUD のテキストを描画。
+            DrawOverlayText();
+
+            base.Draw(gameTime);
+        }
+
+        void DrawOverlayText()
+        {
+            // HUD のテキストを表示。
+            var text = "";
+
+            spriteBatch.Begin();
+
+            spriteBatch.DrawString(spriteFont, text, new Vector2(65, 380), Color.Black);
+            spriteBatch.DrawString(spriteFont, text, new Vector2(64, 380 - 1), Color.Yellow);
+
+            spriteBatch.End();
+        }
+
+        void HandleInput(GameTime gameTime)
+        {
+            float time = (float) gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            lastKeyboardState = currentKeyboardState;
+
+            currentKeyboardState = Keyboard.GetState();
+
+            if (currentKeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+        }
+
+        void UpdateCamera(GameTime gameTime)
+        {
+            float time = (float) gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            float pitch = 0.0f;
+            float yaw = 0.0f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.Up))
+                pitch += time * 0.001f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.Down))
+                pitch -= time * 0.001f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.Left))
+                yaw += time * 0.001f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.Right))
+                yaw -= time * 0.001f;
+
+            camera.Pitch(pitch);
+            camera.Yaw(yaw);
+
+            var movement = new Vector3();
+
+            if (currentKeyboardState.IsKeyDown(Keys.W))
+                movement.Z -= time * 0.1f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.S))
+                movement.Z += time * 0.1f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.A))
+                movement.X -= time * 0.1f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.D))
+                movement.X += time * 0.1f;
+
+            camera.MoveRelative(ref movement);
+
+            if (currentKeyboardState.IsKeyDown(Keys.R))
+            {
+                camera.Position = new Vector3(0, 50, 50);
+                camera.Direction = Vector3.Forward;
+            }
+
+            camera.Update();
+        }
+    }
+
+    #region Program
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MainGame())
+            {
+                game.Run();
+            }
+        }
+    }
+
+    #endregion
+}
