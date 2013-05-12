483728a95f51013f4d4cab11af7928f366aaa004
Will Craftia <willcraftia@gmail.com>
2013-05-12 13:06:11 +0900

    Texture プロパティ削除。

--- Libra.Graphics.Toolkit/DirectTextureDraw.cs
+++ Libra.Graphics.Toolkit/DirectTextureDraw.cs
@@ -30,8 +30,6 @@ namespace Libra.Graphics.Toolkit
 
         public bool Enabled { get; set; }
 
-        public ShaderResourceView Texture { get; set; }
-
         public DirectTextureDraw(Device device)
         {
             if (device == null) throw new ArgumentNullException("device");
@@ -47,8 +45,6 @@ namespace Libra.Graphics.Toolkit
         {
             if (context == null) throw new ArgumentNullException("context");
 
-            context.PixelShaderResources[0] = Texture;
-            context.PixelShaderSamplers[0] = SamplerState.PointClamp;
             context.PixelShader = sharedDeviceResource.PixelShader;
         }
 
