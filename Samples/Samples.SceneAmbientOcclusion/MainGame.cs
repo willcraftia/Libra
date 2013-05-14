7ecdc0d9b12f48d8441d3a6392f708455e776f6d
Will Craftia <willcraftia@gmail.com>
2013-05-14 14:07:07 +0900

    環境光閉塞サンプルの実装の開始。

---
+++ Samples/Samples.SceneAmbientOcclusion/MainGame.cs
@@ -0,0 +1,463 @@
+﻿#region Using
+
+using System;
+using Libra;
+using Libra.Games;
+using Libra.Games.Debugging;
+using Libra.Graphics;
+using Libra.Graphics.Toolkit;
+using Libra.Input;
+using Libra.Xnb;
+
+#endregion
+
+namespace Samples.SceneAmbientOcclusion
+{
+    public sealed class MainGame : Game
+    {
+        /// <summary>
+        /// ウィンドウの幅。
+        /// </summary>
+        const int WindowWidth = 800;
+
+        /// <summary>
+        /// ウィンドウの高さ。
+        /// </summary>
+        const int WindowHeight = 480;
+
+        /// <summary>
+        /// 表示カメラの初期位置。
+        /// </summary>
+        static readonly Vector3 initialCameraPosition = new Vector3(0, 70, 100);
+
+        /// <summary>
+        /// 表示カメラの初期注視点。
+        /// </summary>
+        static readonly Vector3 initialCameraLookAt = Vector3.Zero;
+
+        /// <summary>
+        /// Libra のグラフィックス マネージャ。
+        /// </summary>
+        GraphicsManager graphicsManager;
+
+        /// <summary>
+        /// Libra の XNA コンテント マネージャ。
+        /// </summary>
+        XnbManager content;
+
+        /// <summary>
+        /// スプライト バッチ。
+        /// </summary>
+        SpriteBatch spriteBatch;
+
+        /// <summary>
+        /// フォント。
+        /// </summary>
+        SpriteFont spriteFont;
+
+        /// <summary>
+        /// 中間マップ表示。
+        /// </summary>
+        TextureDisplay textureDisplay;
+
+        /// <summary>
+        /// 表示カメラ。
+        /// </summary>
+        BasicCamera camera;
+
+        /// <summary>
+        /// 前回の更新処理におけるキーボード状態。
+        /// </summary>
+        KeyboardState lastKeyboardState;
+
+        /// <summary>
+        /// 現在の更新処理におけるキーボード状態。
+        /// </summary>
+        KeyboardState currentKeyboardState;
+
+        /// <summary>
+        /// 深度マップの描画先レンダ ターゲット。
+        /// </summary>
+        RenderTarget depthMapRenderTarget;
+
+        /// <summary>
+        /// 法線マップの描画先レンダ ターゲット。
+        /// </summary>
+        RenderTarget normalMapRenderTarget;
+
+        /// <summary>
+        /// 通常シーンの描画先レンダ ターゲット。
+        /// </summary>
+        RenderTarget normalSceneRenderTarget;
+
+        /// <summary>
+        /// ポストプロセス適用後の最終シーン。
+        /// </summary>
+        ShaderResourceView finalSceneTexture;
+
+        /// <summary>
+        /// ポストプロセス。
+        /// </summary>
+        Postprocess postprocess;
+
+        /// <summary>
+        /// ダウン フィルタ パス。
+        /// </summary>
+        DownFilter downFilter;
+
+        /// <summary>
+        /// アップ フィルタ パス。
+        /// </summary>
+        UpFilter upFilter;
+
+        /// <summary>
+        /// ガウシアン ブラー シェーダ。
+        /// </summary>
+        GaussianBlurCore gaussianBlurCore;
+
+        /// <summary>
+        /// ガウシアン ブラー パス (水平)。
+        /// </summary>
+        GaussianBlur gaussianBlurH;
+
+        /// <summary>
+        /// ガウシアン ブラー パス (垂直)。
+        /// </summary>
+        GaussianBlur gaussianBlurV;
+
+        /// <summary>
+        /// 深度マップ エフェクト。
+        /// </summary>
+        DepthMapEffect depthMapEffect;
+
+        /// <summary>
+        /// 法線マップ エフェクト。
+        /// </summary>
+        NormalMapEffect normalMapEffect;
+
+        /// <summary>
+        /// 深度法線マップ エフェクト。
+        /// </summary>
+        DepthNormalMapEffect depthNormalMapEffect;
+
+        /// <summary>
+        /// メッシュ描画のための基礎エフェクト。
+        /// </summary>
+        BasicEffect basicEffect;
+
+        /// <summary>
+        /// 立方体メッシュ。
+        /// </summary>
+        CubeMesh cubeMesh;
+
+        /// <summary>
+        /// 球メッシュ。
+        /// </summary>
+        SphereMesh sphereMesh;
+
+        /// <summary>
+        /// 円柱メッシュ。
+        /// </summary>
+        CylinderMesh cylinderMesh;
+
+        /// <summary>
+        /// 正方形メッシュ。
+        /// </summary>
+        SquareMesh squareMesh;
+
+        public MainGame()
+        {
+            graphicsManager = new GraphicsManager(this);
+
+            content = new XnbManager(Services, "Content");
+
+            graphicsManager.PreferredBackBufferWidth = WindowWidth;
+            graphicsManager.PreferredBackBufferHeight = WindowHeight;
+
+            camera = new BasicCamera
+            {
+                Position = initialCameraPosition,
+                Fov = MathHelper.PiOver4,
+                AspectRatio = (float) WindowWidth / (float) WindowHeight,
+                NearClipDistance = 1.0f,
+                FarClipDistance = 1000.0f
+            };
+            camera.LookAt(initialCameraLookAt);
+            camera.Update();
+
+            textureDisplay = new TextureDisplay(this);
+            Components.Add(textureDisplay);
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(Device.ImmediateContext);
+            spriteFont = content.Load<SpriteFont>("hudFont");
+
+            depthMapRenderTarget = Device.CreateRenderTarget();
+            depthMapRenderTarget.Width = WindowWidth;
+            depthMapRenderTarget.Height = WindowHeight;
+            depthMapRenderTarget.Format = SurfaceFormat.Single;
+            depthMapRenderTarget.DepthFormat = DepthFormat.Depth24Stencil8;
+            depthMapRenderTarget.Initialize();
+
+            normalMapRenderTarget = Device.CreateRenderTarget();
+            normalMapRenderTarget.Width = WindowWidth;
+            normalMapRenderTarget.Height = WindowHeight;
+            normalMapRenderTarget.Format = SurfaceFormat.Vector4;
+            normalMapRenderTarget.DepthFormat = DepthFormat.Depth24Stencil8;
+            normalMapRenderTarget.Initialize();
+
+            normalSceneRenderTarget = Device.CreateRenderTarget();
+            normalSceneRenderTarget.Width = WindowWidth;
+            normalSceneRenderTarget.Height = WindowHeight;
+            normalSceneRenderTarget.DepthFormat = DepthFormat.Depth24Stencil8;
+            normalSceneRenderTarget.Initialize();
+
+            postprocess = new Postprocess(Device.ImmediateContext);
+            postprocess.Width = WindowWidth;
+            postprocess.Height = WindowHeight;
+
+            downFilter = new DownFilter(Device);
+            upFilter = new UpFilter(Device);
+
+            gaussianBlurCore = new GaussianBlurCore(Device);
+            gaussianBlurH = new GaussianBlur(gaussianBlurCore, GaussianBlurPass.Horizon);
+            gaussianBlurV = new GaussianBlur(gaussianBlurCore, GaussianBlurPass.Vertical);
+
+            depthMapEffect = new DepthMapEffect(Device);
+            normalMapEffect = new NormalMapEffect(Device);
+            depthNormalMapEffect = new DepthNormalMapEffect(Device);
+
+            basicEffect = new BasicEffect(Device);
+            basicEffect.AmbientLightColor = new Vector3(0.15f, 0.15f, 0.15f);
+            basicEffect.PerPixelLighting = true;
+            basicEffect.EnableDefaultLighting();
+
+            cubeMesh = new CubeMesh(Device, 20);
+            sphereMesh = new SphereMesh(Device, 20, 32);
+            cylinderMesh = new CylinderMesh(Device, 80, 20, 32);
+            squareMesh = new SquareMesh(Device, 400);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            // キーボード状態およびジョイスティック状態のハンドリング。
+            HandleInput(gameTime);
+
+            // 表示カメラの更新。
+            UpdateCamera(gameTime);
+
+            const float scale = 0.2f;
+            textureDisplay.TextureWidth = (int) (WindowWidth * scale);
+            textureDisplay.TextureHeight = (int) (WindowHeight * scale);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            var context = Device.ImmediateContext;
+
+            // 念のため状態を初期状態へ。
+            context.BlendState = BlendState.Opaque;
+            context.DepthStencilState = DepthStencilState.Default;
+
+            // 深度マップを描画。
+            CreateDepthMap(context);
+
+            // 法線マップを描画。
+            CreateNormalMap(context);
+
+            // 通常シーンを描画。
+            CreateNormalSceneMap(context);
+
+            // ポストプロセスを適用。
+            finalSceneTexture = postprocess.Draw(normalSceneRenderTarget.GetShaderResourceView());
+
+            // 最終的なシーンをバック バッファへ描画。
+            CreateFinalSceneMap(context);
+
+            // HUD のテキストを描画。
+            DrawOverlayText();
+
+            base.Draw(gameTime);
+        }
+
+        void CreateDepthMap(DeviceContext context)
+        {
+            context.SetRenderTarget(depthMapRenderTarget.GetRenderTargetView());
+            context.Clear(Vector4.One);
+
+            DrawScene(context, depthMapEffect);
+
+            context.SetRenderTarget(null);
+
+            // 中間マップ表示。
+            textureDisplay.Textures.Add(depthMapRenderTarget.GetShaderResourceView());
+        }
+
+        void CreateNormalMap(DeviceContext context)
+        {
+            context.SetRenderTarget(normalMapRenderTarget.GetRenderTargetView());
+            context.Clear(Vector4.One);
+
+            DrawScene(context, normalMapEffect);
+
+            context.SetRenderTarget(null);
+
+            // 中間マップ表示。
+            textureDisplay.Textures.Add(normalMapRenderTarget.GetShaderResourceView());
+        }
+
+        void CreateNormalSceneMap(DeviceContext context)
+        {
+            context.SetRenderTarget(normalSceneRenderTarget.GetRenderTargetView());
+            context.Clear(Color.CornflowerBlue);
+
+            DrawScene(context, basicEffect);
+
+            context.SetRenderTarget(null);
+
+            // 中間マップ表示。
+            textureDisplay.Textures.Add(normalSceneRenderTarget.GetShaderResourceView());
+        }
+
+        void DrawPrimitiveMesh(DeviceContext context, PrimitiveMesh mesh, Matrix world, Vector3 color)
+        {
+            basicEffect.DiffuseColor = color;
+
+            DrawPrimitiveMesh(context, mesh, world, color, basicEffect);
+        }
+
+        void DrawScene(DeviceContext context, IEffect effect)
+        {
+            var effectMatrices = effect as IEffectMatrices;
+            if (effectMatrices != null)
+            {
+                effectMatrices.View = camera.View;
+                effectMatrices.Projection = camera.Projection;
+            }
+
+            DrawPrimitiveMesh(context, cubeMesh, Matrix.CreateTranslation(-40, 10, 0), new Vector3(1, 0, 0), effect);
+            DrawPrimitiveMesh(context, sphereMesh, Matrix.CreateTranslation(0, 10, -40), new Vector3(0, 1, 0), effect);
+            for (float z = -180; z <= 180; z += 40)
+            {
+                DrawPrimitiveMesh(context, cylinderMesh, Matrix.CreateTranslation(-180, 40, z), new Vector3(0, 0, 1), effect);
+            }
+            DrawPrimitiveMesh(context, squareMesh, Matrix.Identity, new Vector3(0.5f), effect);
+        }
+
+        void DrawPrimitiveMesh(DeviceContext context, PrimitiveMesh mesh, Matrix world, Vector3 color, IEffect effect)
+        {
+            var effectMatrices = effect as IEffectMatrices;
+            if (effectMatrices != null)
+            {
+                effectMatrices.World = world;
+            }
+
+            var basicEffect = effect as BasicEffect;
+            if (basicEffect != null)
+            {
+                basicEffect.DiffuseColor = color;
+            }
+
+            effect.Apply(context);
+            mesh.Draw(context);
+        }
+
+        void CreateFinalSceneMap(DeviceContext context)
+        {
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Opaque);
+            spriteBatch.Draw(finalSceneTexture, Vector2.Zero, Color.White);
+            spriteBatch.End();
+        }
+
+        void DrawOverlayText()
+        {
+            // HUD のテキストを表示。
+            var text = "";
+
+            spriteBatch.Begin();
+
+            spriteBatch.DrawString(spriteFont, text, new Vector2(65, 320), Color.Black);
+            spriteBatch.DrawString(spriteFont, text, new Vector2(64, 320 - 1), Color.Yellow);
+
+            spriteBatch.End();
+        }
+
+        void HandleInput(GameTime gameTime)
+        {
+            float time = (float) gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            lastKeyboardState = currentKeyboardState;
+            currentKeyboardState = Keyboard.GetState();
+
+            if (currentKeyboardState.IsKeyDown(Keys.Escape))
+                Exit();
+        }
+
+        void UpdateCamera(GameTime gameTime)
+        {
+            float time = (float) gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            float pitch = 0.0f;
+            float yaw = 0.0f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.Up))
+                pitch += time * 0.001f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.Down))
+                pitch -= time * 0.001f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.Left))
+                yaw += time * 0.001f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.Right))
+                yaw -= time * 0.001f;
+
+            camera.Pitch(pitch);
+            camera.Yaw(yaw);
+
+            var movement = new Vector3();
+
+            if (currentKeyboardState.IsKeyDown(Keys.W))
+                movement.Z -= time * 0.1f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.S))
+                movement.Z += time * 0.1f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.A))
+                movement.X -= time * 0.1f;
+
+            if (currentKeyboardState.IsKeyDown(Keys.D))
+                movement.X += time * 0.1f;
+
+            camera.MoveRelative(ref movement);
+
+            if (currentKeyboardState.IsKeyDown(Keys.R))
+            {
+                camera.Position = initialCameraPosition;
+                camera.LookAt(initialCameraLookAt);
+            }
+
+            camera.Update();
+        }
+    }
+
+    #region Program
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MainGame())
+            {
+                game.Run();
+            }
+        }
+    }
+
+    #endregion
+}
