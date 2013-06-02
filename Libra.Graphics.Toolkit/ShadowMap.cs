85b69753ec09714cb5c5fc13282ead1af51e8c5f
Will Craftia <willcraftia@gmail.com>
2013-06-02 19:05:06 +0900

    深度ステンシル生成漏れを修正。

--- Libra.Graphics.Toolkit/ShadowMap.cs
+++ Libra.Graphics.Toolkit/ShadowMap.cs
@@ -152,6 +152,7 @@ namespace Libra.Graphics.Toolkit
                 RenderTarget.Width = size;
                 RenderTarget.Height = size;
                 RenderTarget.Format = format;
+                RenderTarget.DepthFormat = DepthFormat.Depth24Stencil8;
                 RenderTarget.RenderTargetUsage = RenderTargetUsage.Preserve;
                 RenderTarget.Initialize();
             }
