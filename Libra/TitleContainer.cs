dba158cabcac422a49586c4ab87a5573d33be684
Will Craftia <willcraftia@gmail.com>
2013-06-05 01:14:10 +0900

    TitleContainer。

---
+++ Libra/TitleContainer.cs
@@ -0,0 +1,20 @@
+﻿#region Using
+
+using System;
+using System.IO;
+
+#endregion
+
+namespace Libra
+{
+    public static class TitleContainer
+    {
+        static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+        public static Stream OpenStream(string name)
+        {
+            var fullPath = Path.Combine(BaseDirectory, name);
+            return File.OpenRead(fullPath);
+        }
+    }
+}
